4e7ea8bab5bf38fd0e0ed04940370f376c78393f
ofakharany <eomar@me.com>
2025-06-17 07:40:28 -0500

    Update API version to July25 in GraphQL configuration and shopify.server.js

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -44,7 +44,7 @@ class LazyPrismaSessionStorage {
 const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
-  apiVersion: ApiVersion.Unstable,
+  apiVersion: ApiVersion.July25,
   scopes: process.env.SCOPES?.split(","),
   appUrl: process.env.SHOPIFY_APP_URL || "",
   authPathPrefix: "/auth",
@@ -60,7 +60,7 @@ const shopify = shopifyApp({
 });
 
 export default shopify;
-export const apiVersion = ApiVersion.January25;
+export const apiVersion = ApiVersion.July25;
 export const addDocumentResponseHeaders = shopify.addDocumentResponseHeaders;
 export const authenticate = shopify.authenticate;
 export const unauthenticated = shopify.unauthenticated;
